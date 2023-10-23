a47e5ac40dad9186568a94470c087b1fbf9325a3
ziwx123 <uwuourngrlo245@gmail.com>
2023-10-23 15:04:02 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,33 +2,27 @@
 
 int main(void)
 {
-	int d1, d2, d3, d4, d5, d6;
+	int d1, d2, d3;
 
-	for (d1 = 0; d1 <= 9; d1++)
+	for (d1 = 0; d1 <= 7; d1++)
 	{
-		for (d2 = d1 + 1; d2 <= 9; d2++)
+		for (d2 = d1 + 1; d2 <= 8; d2++)
 		{
 			for (d3 = d2 + 1; d3 <= 9; d3++)
 			{
-				for (d4 = d3 + 1; d4 <= 9; d4++)
+				putchar(d1 + '0');
+				putchar(d2 + '0');
+				putchar(d3 + '0');
+				if (d1 != 7 || d2 != 8 || d3 != 9)
 				{
-					for (d5 = d4 + 1; d5 <= 9; d5++)
-					{
-						for (d6 = d5 + 1; d6 <= 9; d6++)
-						{
-							putchar(d1 + '0');
-							putchar(d2 + '0');
-							putchar(d3 + '0');
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
 }
+
